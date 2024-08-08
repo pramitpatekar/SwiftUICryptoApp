8289421d59238d01d887b73559b9dbbb14d0ef18
pramitpatekar <pramitpatekar19@gmail.com>
2024-08-08 13:51:28 +0530

    1

---
+++ CryptoApp/CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Ref on 08/08/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
