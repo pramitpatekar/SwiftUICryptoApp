52db8c5bc9ed248947bddd3f5b6f17a5d7c26adc
pramitpatekar <pramitpatekar19@gmail.com>
2024-08-12 17:20:38 +0530

    7

---
+++ CryptoApp/CryptoApp/Utilities/NetworkingManager.swift
@@ -0,0 +1,53 @@
+//
+//  NetworkingManager.swift
+//  CryptoApp
+//
+//  Created by Ref on 12/08/24.
+//
+
+import Foundation
+import Combine
+
+class NetworkingManager {
+    
+    enum NetworkingError: LocalizedError {
+        case badURLResponse(url: URL)
+        case unknown
+        
+        var errorDescription: String? {
+            switch self {
+            case .badURLResponse(url: let url): "Bad Response from URL: \(url)"
+            case .unknown: "Unknown Error occured"
+                
+            }
+        }
+    }
+    
+    //by making this a static func we never need to initialize the class
+    
+    static func download(url: URL) -> AnyPublisher<Data, any Error> {
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .subscribe(on: DispatchQueue.global(qos: .background))
+            .tryMap({ try handleURLResponse(output: $0, url: url) })
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+    
+    static func handleURLResponse(output: URLSession.DataTaskPublisher.Output, url: URL) throws -> Data {
+        guard let response = output.response as? HTTPURLResponse,
+              response.statusCode >= 200 && response.statusCode < 300 else {
+            throw NetworkingError.badURLResponse(url: url)
+        }
+        return output.data
+    }
+    
+    
+    static func handleCompletion(completion: Subscribers.Completion<Error>) {
+        switch completion {
+        case.finished:
+            break
+        case.failure(let error):
+            print(error.localizedDescription)
+        }
+    }
+}
